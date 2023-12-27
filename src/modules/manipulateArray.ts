182c300348bda1999845980f20e7ec536a57afe5
jessworks <48979913+jessworks@users.noreply.github.com>
2023-12-27 12:32:40 +0100

    basic css
    positioning of elements

---
+++ src/modules/manipulateArray.ts
@@ -0,0 +1,12 @@
+/*
+import { quizQuestions } from './quizData.ts';
+
+function quizQuestionsShuffle() {
+    const questionsShuffled = Math.floor(Math.random() * quizQuestions.length);
+    return quizQuestions[questionsShuffled];
+}
+
+quizQuestionsShuffle();
+
+console.log(quizQuestionsShuffle);
+*/
