fd8bdda3d0c4994b6486df3e6705440973c050c4
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2023-12-27 11:49:04 +0100

    displayQuestion
    add function that display 1 question and 3 answers

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import './scss/style.scss'; // Importera huvud-SCSS-filen
 import typescriptLogo from './assets/images/typescript.svg'; // Exempel på hur ni importerar bilder
 import { sortArrayByText } from './helpers'; // Exempel på hur ni importerar en funktion från en annan fil
+import { displayQuestion } from './modules/displayQuestion';
+
+displayQuestion(0);
 
 /**
  * Här definierar vi en mall för hur vi vill att vår array ska se ut.
---
+++ src/modules/displayQuestion.ts
@@ -0,0 +1,45 @@
+import { quizQuestions } from './quizData';
+
+function displayQuestion(index: number): void {
+  const question = quizQuestions[index];
+  const quizContainer = document.querySelector('#quiz-container');
+
+  if (quizContainer !== null && question !== undefined) {
+    // create an array with the answers
+    const answers = [
+      question.rightAnswer,
+      question.wrongAnswerOne,
+      question.wrongAnswerTwo,
+    ];
+
+    // Shuffle the order of the answers, shuffleArray should be the function that Jessika is made
+    // shuffleArray(answers);
+
+    // Create radio buttons for each answer
+    const answerHTML = answers
+      .map(
+        (answer, i) =>
+          `<input type="radio" name="answer" value="${i}"><label>${answer}</label><br>`)
+      .join('');
+
+    quizContainer.innerHTML = `
+    <h4>${question.question}</h4>
+      <form id="quizForm">
+        ${answerHTML}
+        <br>
+        <button type="button" id="submitBtn">Submit Answer</button>
+      </form>
+    `;
+
+    const submitButton = document.getElementById('submitBtn');
+    if (submitButton !== null) {
+      submitButton.addEventListener('click', () => {
+        // Here should the checkAnswer be to check if answer is correct.
+      });
+    }
+  } else {
+    // here should the function be that show result message when player answered 10 questions
+  }
+}
+
+export { displayQuestion };
