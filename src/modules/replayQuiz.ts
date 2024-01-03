eef146f2ee8cf858a5e1faf15aae50d94015f1d8
armin-164 <arminsadr164@hotmail.com>
2024-01-03 23:53:39 +0100

    Import dislayQuestion, add innertext to btn, add eventlistener to execute displayQuestion on btn

--- src/modules/replayQuiz.ts
+++ src/modules/replayQuiz.ts
@@ -1,5 +1,12 @@
+import { displayQuestion } from './displayQuestion';
+
 function replay(element: HTMLElement):void {
   const replayBtn = document.createElement('button');
+  replayBtn.innerText = 'Play Again';
+  replayBtn.addEventListener('click', () => {
+    displayQuestion(0);
+  });
+  element.appendChild(replayBtn);
 }
 
 
