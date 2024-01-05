cd3b0c09c2f850689af7e32a5ad649737fdde876
Fatima Assi <Fatima.assi@medieinstitutet.se>
2024-01-05 17:51:34 +0100

    WIP:add classes instead of hardcoding in css

--- src/modules/removePage.ts
+++ src/modules/removePage.ts
@@ -1,16 +1,28 @@
+const start: HTMLElement | null = document.querySelector('#start-only');
+const startBtn: HTMLElement | null = document.querySelector('#start-button');
+const progressBarContainer: HTMLElement | null = document.querySelector('#progress-bar-container');
+const quizContainer: HTMLElement | null = document.getElementById('quiz-container');
+
 function hidePage(): void {
-  const start: HTMLElement | null = document.querySelector('#start-only');
-  const startBtn: HTMLElement | null = document.querySelector('#start-button');
-  const progressBarContainer: HTMLElement | null = document.querySelector('#progress-bar-container');
-  const quizContainer: HTMLElement | null =
-    document.getElementById('quiz-container');
+  
 
   if (startBtn !== null && start !== null && quizContainer !== null && progressBarContainer !==  null) {
-    startBtn.addEventListener('click', function () {
-      start.style.display = 'none';
-      quizContainer.style.display = 'block';
-      progressBarContainer.style.display = 'block';
-    });
+    startBtn.addEventListener('click', showQuizContainer);
+ 
+  }
+  
+};
+function showQuizContainer (): void {
+  if (start !== null) {
+    start.classList.add('hidden');
+  }
+  if (quizContainer !== null) {
+    quizContainer.classList.add('block');
+   
+  }
+  if (progressBarContainer !== null) {
+    progressBarContainer.classList.add('block');  
+    
   }
 };
 
