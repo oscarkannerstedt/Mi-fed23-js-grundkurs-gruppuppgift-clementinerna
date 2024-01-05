808f36c301587f0c2436c7f44929a5baae9b6745
armin-164 <arminsadr164@hotmail.com>
2024-01-05 22:02:14 +0100

    Add condition to check how many games have been played
    Create a variable that will contain the number of games played.
    Add an if statement that if gamesplayed is less than 1, the replay logic
    will be executed.

--- src/modules/replayQuiz.ts
+++ src/modules/replayQuiz.ts
@@ -1,14 +1,19 @@
 import { displayQuestion, currentQuestionIndex } from './displayQuestion';
 import { initializeProgressBar } from './progressBar';
 
+let gamesPlayed = 0;
+
 function replay(element: HTMLElement):void {
-  const replayBtn = document.createElement('button');
-  replayBtn.innerText = 'Play Again';
-  replayBtn.addEventListener('click', () => {
-    displayQuestion(currentQuestionIndex);
-    initializeProgressBar();
-  });
-  element.appendChild(replayBtn);
+  if (gamesPlayed < 1) {
+    const replayBtn = document.createElement('button');
+    replayBtn.innerText = 'Play Again';
+    replayBtn.addEventListener('click', () => {
+      displayQuestion(currentQuestionIndex);
+      initializeProgressBar();
+      gamesPlayed += 1;
+    });
+    element.appendChild(replayBtn);
+  } 
 }
 
 
