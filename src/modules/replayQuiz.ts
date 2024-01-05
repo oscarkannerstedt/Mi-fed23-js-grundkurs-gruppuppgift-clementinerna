90aba9fb5a1798db047e1cc7d165fceb5322afaa
armin-164 <arminsadr164@hotmail.com>
2024-01-05 17:05:50 +0100

    import initializeprogressbar and execute it to reset progressbar

--- src/modules/replayQuiz.ts
+++ src/modules/replayQuiz.ts
@@ -1,10 +1,12 @@
 import { displayQuestion, currentQuestionIndex } from './displayQuestion';
+import { initializeProgressBar } from './progressBar';
 
 function replay(element: HTMLElement):void {
   const replayBtn = document.createElement('button');
   replayBtn.innerText = 'Play Again';
   replayBtn.addEventListener('click', () => {
     displayQuestion(currentQuestionIndex);
+    initializeProgressBar();
   });
   element.appendChild(replayBtn);
 }
