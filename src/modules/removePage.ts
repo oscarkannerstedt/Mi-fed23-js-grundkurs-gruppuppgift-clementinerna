378c99b80c8370e820036b3b274bb16b3a8a431c
Fatima Assi <Fatima.assi@medieinstitutet.se>
2024-01-11 11:29:59 +0100

    fix: Add class and edit function

--- src/modules/removePage.ts
+++ src/modules/removePage.ts
@@ -3,17 +3,21 @@ function hidePage(): void {
   const startBtn: HTMLElement | null = document.querySelector('#start-button');
   const progressBarContainer: HTMLElement | null = document.querySelector('#progress-bar-container');
   const quizContainer: HTMLElement | null = document.getElementById('quiz-container');
+  const stopWatch: HTMLElement | null = document.querySelector('#stopwatch');
 
-  if (startBtn !== null && start !== null && quizContainer !== null && progressBarContainer !==  null) {
+  if (startBtn !== null && start !== null && quizContainer !== null && progressBarContainer !==  null 
+    && stopWatch !==  null ) {
     startBtn?.addEventListener('click', function showQuizContainer () {
       start.classList.remove('grid');
       start.classList.add('hidden');
 
       quizContainer.classList.remove('hidden');
       progressBarContainer.classList.remove('hidden');
+      stopWatch.classList.remove('hidden');
 
     });
-  }
+  } 
 }
 
 export { hidePage };
+//
--- src/modules/replayQuiz.ts
+++ src/modules/replayQuiz.ts
@@ -4,6 +4,7 @@ import { initializeProgressBar } from './progressBar';
 function replay(element: HTMLElement):void {
   const replayBtn = document.createElement('button');
   replayBtn.innerText = 'Play Again';
+  replayBtn.classList.add('replayBtn');
   replayBtn.addEventListener('click', () => {
     displayQuestion(currentQuestionIndex);
     initializeProgressBar();
